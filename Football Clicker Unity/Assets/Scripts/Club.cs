278abf0cbbcade9af8c50dfda593a66b9f503d8d
Thomas Stone <lotsgon@hotmail.com>
2018-11-05 04:04:13 +0000

    Inital Unity commit.
    Got a solid start to the clicker game with wait times and money updating on the UI.

---
+++ Football Clicker Unity/Assets/Scripts/Club.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Club : MonoBehaviour {
+
+    public string Name { get; private set; }
+    public string LongName { get; private set; }
+    public string SixLetterName { get; private set; }
+    public string Nation { get; private set; }
+    public string City { get; private set; }
+    public int YearFounded { get; private set; }
+    public Vector3 TeamColour { get; private set; }
+    public float Money { get; private set; } = 100f;
+    public long Value { get; private set; }
+
+    public UnityEngine.UI.Text MoneyDisplay;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoneyDisplay.text = $"Money: {this.Money}";
+    }
+
+    // Update is called once per second
+    void FixedUpdate () {
+
+    }
+
+    public void SetMoney(float money)
+    {
+        Money += money;
+    }
+}
---
+++ Football Clicker Unity/Assets/Scripts/UpgradeableClickerObject.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class UpgradeableClickerObject : MonoBehaviour {
+
+    public int UpgradeLevel { get; private set; } = 0;
+    public bool IsEnabled { get; private set; } = false;
+    public float UpgradeCost { get { return mUpgradeCost; } private set { mUpgradeCost = value; } }
+    public float TimeUntilIncome { get; private set; } = 0;
+    public bool CanUpgrade { get; private set; } = true;
+    public float Income { get; private set; } = 0f;
+    public UnityEngine.UI.Text Info;
+
+    [SerializeField]
+    private float mUpgradeCost = 0f;
+    [SerializeField]
+    private Club mClub;
+    private float mNewCost = 0f;
+
+    public UpgradeableClickerObject(float upgradeCost, Club club)
+    {
+        club = mClub;
+        upgradeCost = mUpgradeCost;
+        Income = Mathf.Round(mUpgradeCost * 1.15f);
+        TimeUntilIncome = Mathf.Round(mUpgradeCost * 0.03f);
+    }
+
+	// Use this for initialization
+	void Start () {
+        Income = Mathf.Round(mUpgradeCost * 0.05f);
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        Info.text = $"Cost:{UpgradeCost} Time Until: {TimeUntilIncome}";   
+    }
+
+    // Update is called once per second
+    void FixedUpdate () {
+
+        if (TimeUntilIncome > 0)
+        {
+            TimeUntilIncome -= 1f;
+            //mClub.SetMoney(Income);
+            //TimeUntilIncome = Mathf.Round(mUpgradeCost * 0.03f);
+        }
+	}
+
+    public void OnClick()
+    {
+        if (TimeUntilIncome <= 0)
+        {
+            mClub.SetMoney(-mUpgradeCost);
+            mUpgradeCost = Mathf.Round(mUpgradeCost * 1.15f);
+            mNewCost = Mathf.Pow(mUpgradeCost, mNewCost = mUpgradeCost);
+            Income = Mathf.Round(mUpgradeCost * 0.05f);
+            TimeUntilIncome = Mathf.Round(mUpgradeCost * 0.03f);
+        }
+    }
+}
